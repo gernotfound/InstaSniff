62468817ffb0007d3de0f20c163d64465c061394
Gerardo Califano <califano.ger@gmail.com>
2026-09-11 08:50:25 +0200

    test: target dedicated E2E file pattern

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  testMatch: '**/*.e2e.mjs',
   fullyParallel: false,
   retries: process.env.CI ? 1 : 0,
   workers: 1,
